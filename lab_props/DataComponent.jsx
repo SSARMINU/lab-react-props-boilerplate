ce401e507937daa6904e4be66e2ee8529ebaa301
SSARMINU <127767691+SSARMINU@users.noreply.github.com>
2024-05-23 20:59:00 +0530

    Update DataComponent.jsx

--- lab_props/DataComponent.jsx
+++ lab_props/DataComponent.jsx
@@ -1,32 +1,13 @@
-import React from 'react';
-import App from './App';
-import AppClass from './AppClass';
-import elephant from './images/elephant.jpeg';
-const imageData = [
-  {
-    id: 1,
-    img: elephant
-  },
-  {
-    id: 2,
-    img: elephant
-  },
-  {
-    id: 3,
-    img: elephant
-  },
-  {
-    id: 4,
-    img: elephant
-  }
-];
+import elephant from "../images/elephant.jpeg";
 
-function MainApp() {
-  return (
-    <div>
-      <App data={imageData} />
-    </div>
-  );
-}
+const imageData = () => {
+  let data = [
+    { id: 1, img: elephant },
+    { id: 2, img: elephant },
+    { id: 3, img: elephant },
+    { id: 4, img: elephant }
+  ];
+  return data;
+};
 
-export default MainApp;
+export default imageData;  
